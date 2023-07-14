6081a0b2977ddff1615a16f09ead8b1ff47319b7
Nathanael <natsiewks@gmail.com>
2023-07-15 01:40:48 +0800

    Add courseinfo into single object

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,45 +1,29 @@
-const Header = (props) => {
-  return (
-    <div>
-      <h1>{props.course}</h1>
-    </div>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <div>
-      <p>{props.part} {props.exercises}</p>
-    </div>
-  )
-}
-
-const Content = () => {
-  return (
-    <div>
-      <Part part='Fundamentals of React' exercises={10} />
-      <Part part='Using props to pass data' exercises={7} />
-      <Part part='State of a component' exercises={14} />
-    </div>
-  )
-}
-
-const Total = () => {
-  return (
-    <div>
-      <p>Number of exercises {10+7+14}</p>
-    </div>
-  )
-}
-
 const App = () => {
-  const course = 'Half Stack application development'
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content />
-      <Total />
+      <h1>{course.name}</h1>
+      <p>{course.parts[0].name} {course.parts[0].exercises}</p>
+      <p>{course.parts[1].name} {course.parts[1].exercises}</p>
+      <p>{course.parts[2].name} {course.parts[2].exercises}</p>
+      <p>Number of exercises {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}</p>
     </div>
   )
 }
