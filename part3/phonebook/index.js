a5cca30cfef4ee26d34b619cfecc01f2e6b6b648
Nathanael <natsiewks@gmail.com>
2023-07-22 23:01:02 +0800

    Backend for phonebook

---
+++ part3/phonebook/index.js
@@ -0,0 +1,105 @@
+const express = require('express')
+var morgan = require('morgan')
+const app = express()
+const cors = require('cors')
+
+let persons = 
+[
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+const generateId = () => {
+    return Math.floor(Math.random() * 999999999)
+}
+
+morgan.token('obj', (request, response) => {
+    return JSON.stringify(request.body)
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :obj'))
+app.use(express.json())
+app.use(cors())
+
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+  })
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    }
+    else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+    response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'name or number missing'
+        })
+    }
+
+    else if (persons.find(person => person.name === body.name)) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId(),
+    }
+
+    persons = persons.concat(person)
+
+    morgan.obj(request, response)
+
+    response.json(person)
+})
+
+app.get('/info', (request, response) => {
+    date = new Date()
+    console.log(persons.length)
+    console.log(request.body)
+    response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
